9e9b2ca81a225196044e292212453d4689808dc5
Aditya  Sinha <u6010425@anu.edu.au>
2017-12-12 19:29:48 +1100

    Adding everything else

---
+++ entertainment_center.py
@@ -0,0 +1,35 @@
+import media
+import fresh_tomatoes
+
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",  # noqa
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+avatar = media.Movie("Avatar",
+                     "A marine on an alien planet",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",  # noqa
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+tdkr = media.Movie("The Dark Knight Rises",
+                   "Batman returns to save Gotham from Bane",
+                   "https://upload.wikimedia.org/wikipedia/en/8/83/Dark_knight_rises_poster.jpg",  # noqa
+                   "https://www.youtube.com/watch?v=g8evyE9TuYk")
+
+hugo = media.Movie("Hugo",
+                   "an orphan is wrapped up in a mystery involving his late father and an automaton",  # noqa
+                   "https://upload.wikimedia.org/wikipedia/en/7/73/Hugo_Poster.jpg",  # noqa
+                   "https://www.youtube.com/watch?v=Hv3obL9HqyY")
+
+fight_club = media.Movie("Fight Club",
+                         "An insomniac office worker joins an underground fight club",  # noqa
+                         "https://upload.wikimedia.org/wikipedia/en/f/fc/Fight_Club_poster.jpg",  # noqa
+                         "https://www.youtube.com/watch?v=BdJKm16Co6M")
+
+the_matrix = media.Movie("The Matrix",
+                         "A computer hacker learns about the true nature of his reality",  # noqa
+                         "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",  # noqa
+                         "https://www.youtube.com/watch?v=m8e-FF8MsqU")
+
+movies = [toy_story, avatar, tdkr, hugo, fight_club, the_matrix]
+fresh_tomatoes.open_movies_page(movies)
